742bdedb4c78284b5bc66173c991e5bf4e99e274
11t518s <11t518s20161759@gmail.com>
2023-12-10 23:31:43 +0900

    12.10

--- color-button/src/App.test.tsx
+++ color-button/src/App.test.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { fireEvent } from "@testing-library/react";
 
-test('renders learn react link', () => {
+test("button has correct initial text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const colorButton = screen.getByRole("button");
+
+  expect(colorButton).toHaveTextContent("Change to blue");
+});
+
+test("button has correct initial color", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+});
+
+test("button turns color and text", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  fireEvent.click(colorButton);
+
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+  expect(colorButton).toHaveTextContent("Change to red");
 });
--- color-button/src/App.tsx
+++ color-button/src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [isClick, setIsClick] = useState(false);
+
+  const handleClick = () => setIsClick((prev) => !prev);
+
+  const displayText = isClick ? "Change to red" : "Change to blue";
+  const displayStyle = isClick
+    ? { backgroundColor: "blue" }
+    : { backgroundColor: "red" };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleClick} style={displayStyle}>
+        {displayText}
+      </button>
     </div>
   );
 }
